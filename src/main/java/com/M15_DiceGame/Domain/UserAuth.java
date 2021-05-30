832d37219b7bbfdc51f1b9b4afc2b04c5e090e1c
merdaalcanto <merdaalcanto@hotmail.com>
2021-05-30 20:52:15 +0200

    Added functionality for secure access with JWT.
    Accessible URI without authentication nor token:  localhost:8080/user which generates a Token for the user needed to access the other URIs.
    
    All other URIs localhost:8080/players and so on require the token to access.

---
+++ src/main/java/com/M15_DiceGame/Domain/UserAuth.java
@@ -0,0 +1,48 @@
+package com.M15_DiceGame.Domain;
+
+public class UserAuth {
+	
+	private String user;
+	private String pwd;
+	private String token;
+	
+	
+	/**
+	 * @return the user
+	 */
+	public String getUser() {
+		return user;
+	}
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(String user) {
+		this.user = user;
+	}
+	/**
+	 * @return the pwd
+	 */
+	public String getPwd() {
+		return pwd;
+	}
+	/**
+	 * @param pwd the pwd to set
+	 */
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	/**
+	 * @return the token
+	 */
+	public String getToken() {
+		return token;
+	}
+	/**
+	 * @param token the token to set
+	 */
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+}
